14da0bf2d57e4de9b3ce3f026e35160c45768021
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2023-07-13 10:59:44 +0700

    fixed top navbar

--- src/js/main.js
+++ src/js/main.js
@@ -9,15 +9,36 @@ $(document).ready(function () {
         }
     });
 
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('#back-to-top').addClass('show');
-        } else {
-            $('#back-to-top').removeClass('show');
-        }
-    });
-
     $('#back-to-top').click(function () {
         $('html, body').animate({ scrollTop: 0 }, 1);
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1) {
+            $('#back-to-top').addClass('show');
+            $('#navbar').addClass('shadow');
+        } else {
+            $('#back-to-top').removeClass('show');
+            $('#navbar').removeClass('shadow');
+        }
+
+        const navLinks = $('#navbar ul.nav li a');
+        navLinks.each(function () {
+            const target = $(this).attr('href');
+            const targetElement = $(target);
+            if (targetElement.length) {
+                const targetPosition = targetElement.offset().top;
+                const windowHeight = $(window).height();
+                const scrollPosition = $(window).scrollTop();
+                if (
+                    targetPosition <= scrollPosition + windowHeight &&
+                    targetPosition + targetElement.outerHeight() >
+                        scrollPosition
+                ) {
+                    navLinks.removeClass('active-nav-link');
+                    $(this).addClass('active-nav-link');
+                }
+            }
+        });
     });
 });
